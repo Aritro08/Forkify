0a3bb0f326fbd4a8c3c44525422399f7f6cee603
Aritro Pal <aritro.y2k@gmail.com>
2019-10-30 15:48:14 +0530

    Add search results and pagination

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,37 @@
-import x from './test';
-console.log(`Imported ${x} from another module`);
+import Search from './models/Search';
+import {elements, renderLoader, removeLoader} from './views/base';
+import * as searchView from './views/searchView';
+
+const state = {};
+
+const controlSearch = async () => {
+    const query = searchView.getInput();
+    if(query)
+    {
+        state.search = new Search(query);
+
+        renderLoader(elements.searchDiv);
+        searchView.clearInput();
+        searchView.clearResults();
+
+        await state.search.getResult();
+
+        removeLoader();
+        searchView.renderResults(state.search.results);
+    }
+}
+
+elements.searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+    controlSearch();
+});
+
+elements.searchPageBtn.addEventListener('click', event =>{
+    const btn = event.target.closest('.btn-inline');
+    if(btn)
+    {
+        const goTo = parseInt(btn.dataset.goto);
+        searchView.clearResults();
+        searchView.renderResults(state.search.results, goTo);
+    }
+});
---
+++ src/js/views/base.js
@@ -0,0 +1,25 @@
+export const elements = {
+    searchForm: document.querySelector('.search'),
+    searchInput: document.querySelector('.search__field'),
+    searchResList: document.querySelector('.results__list'),
+    searchDiv: document.querySelector('.results'),
+    searchPageBtn: document.querySelector('.results__pages')
+}
+
+export const renderLoader = parent => {
+    const loader = `
+        <div class = "loader">
+            <svg>
+                <use href="img/icons.svg#icon-cw"></use>
+            </svg>
+        </div>`;
+    parent.insertAdjacentHTML('afterbegin', loader);
+};
+
+export const removeLoader = () => {
+    const loader = document.querySelector('.loader');
+    if(loader)
+    {
+        loader.parentElement.removeChild(loader);
+    }
+}
